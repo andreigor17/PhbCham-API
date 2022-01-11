a9e18fc5d39614b3f2944ddfcb82d4fb3a98df40
André Igor <andreigor.17@hotmail.com>
2022-01-10 23:24:10 -0300

    Criando metodo de busca

--- src/main/java/com/irontech/phbchamp/api/controller/PlayerController.java
+++ src/main/java/com/irontech/phbchamp/api/controller/PlayerController.java
@@ -3,7 +3,10 @@ package com.irontech.phbchamp.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +22,19 @@ public class PlayerController {
 	public CadastroPlayerService cadastroPlayer;
 	@Autowired
 	public PlayerRepository playerRepository;
-	
+
 	@GetMapping
-	public List<Player> players(){
+	public List<Player> players() {
 		return playerRepository.listar();
+	}
+
+	@GetMapping("/{playerId}")
+	public ResponseEntity<Player> buscar(@PathVariable Long playerId) {
+		Player player = playerRepository.buscar(playerId);
+		if (player != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(player);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
