6f22339517658de333de2de9694cd1b2cf97403f
André Igor <andreigor.17@hotmail.com>
2022-01-10 21:52:56 -0300

    Criando repositorio

---
+++ src/main/java/com/irontech/phbchamp/PhbChampApiApplication.java
@@ -0,0 +1,13 @@
+package com.irontech.phbchamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhbChampApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhbChampApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irontech/phbchamp/PhbChampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irontech.phbchamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhbChampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
