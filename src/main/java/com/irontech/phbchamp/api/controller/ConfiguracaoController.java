5beb2a22ae9658eb410373a3f49550b7f54c29f9
André Igor <andreigor.17@hotmail.com>
2022-04-15 00:12:37 -0300

    Removendo imports

--- src/main/java/com/irontech/phbchamp/api/controller/ConfiguracaoController.java
+++ src/main/java/com/irontech/phbchamp/api/controller/ConfiguracaoController.java
@@ -6,7 +6,6 @@
 package com.irontech.phbchamp.api.controller;
 
 import com.irontech.phbchamp.domain.model.Configuracao;
-import com.irontech.phbchamp.domain.model.Player;
 import com.irontech.phbchamp.domain.repository.ConfiguracaoRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
